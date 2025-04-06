0d8d85df2570c0ced3087cb075f3d40c916a5551
Shivanjali Verma <shivanverma@microsoft.com>
2025-04-06 16:31:17 +0530

    added filters

---
+++ filters/filters.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+
+/// <summary>
+/// Example of function invocation filter to perform logging before and after function invocation.
+/// </summary>
+public class LoggingFilter(ILogger logger) : IFunctionInvocationFilter
+{
+    public async Task OnFunctionInvocationAsync(FunctionInvocationContext context, Func<FunctionInvocationContext, Task> next)
+    {
+        logger.LogInformation("FunctionInvoking - {PluginName}.{FunctionName}", context.Function.PluginName, context.Function.Name);
+
+        await next(context);
+
+        logger.LogInformation("FunctionInvoked - {PluginName}.{FunctionName}", context.Function.PluginName, context.Function.Name);
+    }
+}
+
+/// <summary>
+/// Example of function invocation filter to perform input validation before function invocation.
+/// </summary>
+public class InputValidationFilter : IFunctionInvocationFilter
+{
+    public async Task OnFunctionInvocationAsync(FunctionInvocationContext context, Func<FunctionInvocationContext, Task> next)
+    {
+        var input = context.Arguments.ToString();
+
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            throw new ArgumentException("Input cannot be empty.");
+        }
+
+        await next(context);
+    }
+}
+
+/// <summary>
+/// Example of prompt render filter which overrides rendered prompt before sending it to AI.
+/// </summary>
+public class SafePromptFilter : IPromptRenderFilter
+{
+    public async Task OnPromptRenderAsync(PromptRenderContext context, Func<PromptRenderContext, Task> next)
+    {
+        // Example: get function information
+        var functionName = context.Function.Name;
+
+        await next(context);
+
+        // Example: override rendered prompt before sending it to AI
+        context.RenderedPrompt = "Safe prompt";
+    }
+}
